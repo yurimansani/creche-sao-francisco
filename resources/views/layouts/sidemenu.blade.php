109ca99b99bac7e34fa7b9fb85d6b622d8aba984
Yuri Mansani Vieira <yurimv9@gmail.com>
2019-11-23 16:56:29 -0300

    add dist and plugins

---
+++ resources/views/layouts/sidemenu.blade.php
@@ -0,0 +1,72 @@
+<li class="nav-header">Cadastros</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="fas fa-place-of-worship"></i>
+        <p>
+            Eventos
+        </p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="far fa-building"></i>
+        <p>
+            Projetos
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="fas fa-home"></i>
+        <p>
+            Apartamentos
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="fas fa-tools"></i>
+        <p>
+            Fornecedores
+        </p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="fas fa-dollar-sign"></i>
+        <p>
+            Faturas
+        </p>
+    </a>
+</li>
+
+{{--<li class="nav-item">--}}
+{{--    <a href="{{ route('admin.messages.index') }}" class="nav-link">--}}
+{{--        <i class="nav-icon far fa-calendar-alt"></i>--}}
+{{--        <p>--}}
+{{--            Mensagens--}}
+{{--        </p>--}}
+{{--    </a>--}}
+{{--</li>--}}
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="fas fa-user-tie"></i>
+        <p>
+            Proprietários
+        </p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="far fa-user"></i>
+        <p>
+            Inquilinos
+        </p>
+    </a>
+</li>
