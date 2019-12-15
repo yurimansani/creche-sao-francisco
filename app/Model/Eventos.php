2df648646a1b8d5e6c6cf99d7735dcb61c65166a
Yuri Mansani Vieira <yurimv9@gmail.com>
2019-12-15 17:48:29 -0300

    criando a model eventos
    ainda nao ta concluida

--- app/Model/Eventos.php
+++ app/Model/Eventos.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Eventos extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'events';
+
+
 }
